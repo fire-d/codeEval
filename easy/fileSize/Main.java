556574dd0ae1d78481e061f5b9b6a052e09bb464
fire-d <maximiliano.gigena@outlook.com.ar>
2016-08-22 20:52:29 -0300

    Create Main.java

---
+++ easy/fileSize/Main.java
@@ -0,0 +1,9 @@
+/* Sample code to read in test cases:*/
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    File file = new File(args[0]);
+    System.out.println(file.length());
+  }
+}
